2c4cf56ed1ddd07152e7d0efed0c4defd55f1678
nsivaaws <cloud.sivakrishna@gmail.com>
2018-07-31 03:28:52 -0700

    test

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -25,12 +25,7 @@ SECRET_KEY = '4d!2@*cmd5)xz$s#jt9xt8dcox2om!78a^=+wy!dyyf)9b!lkj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'testserver',
-    'localhost'
-]
-
-
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
